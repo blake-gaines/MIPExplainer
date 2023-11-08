24426bf6133b5afb878adf32955fb2c57d87981d
blake <blake@uconn.edu>
2023-11-08 15:25:06 -0500

    Paths as Strings

--- arg_parser.py
+++ arg_parser.py
@@ -1,5 +1,4 @@
 import argparse
-import pathlib
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -9,7 +8,7 @@ def parse_args():
     parser.add_argument("-m", "--max_class", type=int, required="True", help="Index of logit to be maximized")
     sol_init_args.add_argument("-n","--num_nodes", type=int, help="Number of nodes in the explanation graph")
     
-    parser.add_argument("--model_path", type=pathlib.Path, help="Path to model file")
+    parser.add_argument("--model_path", type=str, help="Path to model file")
 
     parser.add_argument("-r", "--regularizers", type=str, action="extend", choices=["Cosine", "Squared L2", "L2"], default=[], help="Names of regularizers to apply")
     parser.add_argument("--regularizer_weights", type=float, action="extend", default=[], help="Regularizer weights, with order corresponding to the names provided to the 'regularizers' argument")
@@ -19,7 +18,7 @@ def parse_args():
     parser.add_argument("--init_with_data", action="store_true", help="If true, initialize with the graph in the dataset (index of init_index), otherwise start with a predefined graph")
     sol_init_args.add_argument("--init_index", type=int, help="Index of initialization graph")
 
-    parser.add_argument("-o", "--output_file", type=pathlib.Path, default="./solutions.pkl", help="Name of output file")
+    parser.add_argument("-o", "--output_file", type=str, default="./solutions.pkl", help="Name of output file")
     parser.add_argument("-p", "--param_file", type=str, default="./tune0.prm", help="Name of file containing solver parameters")
     parser.add_argument("--log", action='store_true', help="Log the run with Weights & Biases")
     parser.add_argument('--no-log', dest='log', action='store_false')
