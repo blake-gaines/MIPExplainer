a15d0926651cbba4b91e62432efe0641305186e7
blake <blake@uconn.edu>
2023-11-30 11:56:39 -0500

    MUTAG Enums

--- datasets/MUTAG.py
+++ datasets/MUTAG.py
@@ -2,10 +2,50 @@ from .dataset import GraphDataset
 from torch_geometric.datasets import TUDataset
 import os
 
+
 class MUTAG_dataset(GraphDataset):
+    NODE_CLS = {
+        0: "C",
+        1: "N",
+        2: "O",
+        3: "F",
+        4: "I",
+        5: "Cl",
+        6: "Br",
+    }
+
+    NODE_COLOR = {
+        0: "lightgray",
+        1: "deepskyblue",
+        2: "red",
+        3: "cyan",
+        4: "magenta",
+        5: "springgreen",
+        6: "chocolate",
+    }
+
+    GRAPH_CLS = {
+        0: "nonmutagen",
+        1: "mutagen",
+    }
+
+    EDGE_CLS = {
+        0: "aromatic",
+        1: "single",
+        2: "double",
+        3: "triple",
+    }
+
+    EDGE_WIDTH = {
+        0: 3,
+        1: 2,
+        2: 4,
+        3: 6,
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def get_data(self):
-        dataset = TUDataset(root=os.path.join(self.root, "TUDataset"), name='MUTAG')
+        dataset = TUDataset(root=os.path.join(self.root, "TUDataset"), name="MUTAG")
         return dataset
